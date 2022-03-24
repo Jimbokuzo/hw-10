c6faec4ca9dad908b50a7318c7050d0aeea5862c
grunsky yegor <chopgruny@gmail.com>
2022-03-24 11:34:11 +0200

    1

--- main.js
+++ main.js
@@ -2,116 +2,105 @@
 var users = [{
 
   "id": 1,
-
+  
   "name": "Veronika",
-
+  
   "email": "vgroves0@vistaprint.com",
-
+  
   "age": 50,
-
+  
   "city": "Javhlant",
-
+  
   "gender": "Female",
-
+  
   "inn": 2604048463
-
+  
   }, {
-
+  
   "id": 2,
-
+  
   "name": "Correy",
-
+  
   "email": "cskidmore1@shop-pro.jp",
-
+  
   "age": 50,
-
+  
   "city": "Junglinster",
-
+  
   "gender": "Male",
-
+  
   "inn": 5396152028
-
+  
   }, {
-
+  
   "id": 3,
-
+  
   "name": "Chrissie",
-
+  
   "email": "csobieski2@go.com",
-
+  
   "age": 40,
-
+  
   "city": "Mercaderes",
-
+  
   "gender": "Female",
-
+  
   "inn": 7437115687
-
+  
   }, {
-
+  
   "id": 4,
-
+  
   "name": "Adrianna",
-
+  
   "email": "awharrier3@hud.gov",
-
+  
   "age": 33,
-
+  
   "city": "Manuel Cavazos Lerma",
-
+  
   "gender": "Non-binary",
-
+  
   "inn": 6292774004
-
+  
   }, {
-
+  
   "id": 5,
-
+  
   "name": "Fairlie",
-
+  
   "email": "feliasen4@bing.com",
-
+  
   "age": 34,
-
+  
   "city": "Радовиш",
-
+  
   "gender": "Female",
-
+  
   "inn": 5943534093
+  
   }]
 
 var contentHTML = document.querySelector(".content")
 accordeon(users, contentHTML)
-toggle(contentHTML)
 
-function toggle() {
+function toggle(){
   var accordeonBody = document.querySelector(".accordion-dropdown")
-  var content = accordeonBody.classList.contains("hide")
-  if (content) {
-    accordeonBody.classList.replace("hide", "show")
-  } else {
-    accordeonBody.classList.replace("show", "hide")
-  }
-
+  accordeonBody.classList.toggle('show')
   var accordeonArrow = document.querySelector(".arrow")
-  var arrowHTML = accordeonArrow.classList.contains("close")
-  if (arrowHTML) {
-    accordeonArrow.classList.replace("close", "open")
-  } else {
-    accordeonArrow.classList.replace("open", "close")
-  }
+  accordeonArrow.classList.toggle('open')
 }
 
-function accordeon(data, node) {
+function accordeon(data, node){
   for (var user of data) {
     var sectionHTML = document.createElement("section")
     sectionHTML.className = "accordion"
     sectionHTML.innerHTML = `  
       <div class="accordion-header">
         <h2>${user.name}</h2>
-        <div><img class="arrow close" src="./img/arrow.png" alt=""></div>
+        <img class="arrow" src="./img/arrow.png" alt="">
       </div>
-      <div class="accordion-dropdown hide main">
+      <div class="accordion-dropdown main">
           <div class="avatar"></div>
           <div class="block-content">
             <div class="inn"><p>inn</p>: ${user.inn}</div>
